6d115e01707511d6d50c2717377fbbc3e15d9d1f
micheed <mnzamununu@gmail.com>
2026-04-27 01:11:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-var CACHE = 'ctct-songbook-v4';
+var CACHE = 'ctct-songbook-v5';
 var FILES = [
   '/ctct-songbook/',
   '/ctct-songbook/index.html',
   '/ctct-songbook/songs_final.json',
   '/ctct-songbook/manifest.json'
 ];
-
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', function(e) {
   e.waitUntil(caches.keys().then(function(keys) {
     return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
   }));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', function(e) {
   e.respondWith(caches.match(e.request).then(function(r) {
     return r || fetch(e.request).then(function(res) {
