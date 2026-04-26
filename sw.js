7e51eb6c64306939d1376beca93cc2feb39eb4e1
micheed <mnzamununu@gmail.com>
2026-04-26 18:31:49 +0200

    SongBook

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE = 'ctct-songbook-v1';
+var FILES = ['/', '/index.html', '/songs_final.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) {
+    return r || fetch(e.request).then(function(res) {
+      var clone = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+      return res;
+    });
+  }).catch(function() { return caches.match('/index.html'); }));
+});
